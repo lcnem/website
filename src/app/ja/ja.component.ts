1da542b15425a3422ee3faeca4aa74b49eb643b4
Dragon-taro <o_o_o_o_o1031@yahoo.co.jp>
2018-06-24 14:39:27 +0900

    flexの調整

--- src/app/ja/ja.component.ts
+++ src/app/ja/ja.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ja',
   templateUrl: './ja.component.html',
-  styleUrls: ['./ja.component.css']
+  styleUrls: ['./ja.component.scss']
 })
 export class JaComponent implements OnInit {
 
