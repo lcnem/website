beec0dc41c896ad7fd65c90c289374100c8ac41a
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-06-25 11:56:32 +0900

    update

--- src/app/en/en.component.ts
+++ src/app/en/en.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-en',
   templateUrl: './en.component.html',
-  styleUrls: ['./en.component.css']
+  styleUrls: ['./en.component.scss']
 })
 export class EnComponent implements OnInit {
 
