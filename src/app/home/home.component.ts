f4bb7852533acbbc15e019cef9f4791cf0e5ef30
ryuseikurata <amnosmusic13@gmail.com>
2018-08-25 19:54:28 +0900

    [update]エラー修正

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -70,7 +70,7 @@ LCNEM Wallet connects the Google Account with the private key, so it is possible
             ja: "自家型前払式支払手段として発行開始"
         },
         issueAsPrepaidTooltip: {
-            en: "We will start issuing as a self-contained prepaid payment method. In "self-contained prepaid payment method", the cash is restricted by law and it has been only in exchange with Amazon gift certificates.",
+            en: "We will start issuing as a self-contained prepaid payment method. In self-contained prepaid payment method, the cash is restricted by law and it has been only in exchange with Amazon gift certificates.",
             ja: `自家型前払式支払手段として、発行を開始します。「自家型前払式支払手段」では、法律により換金が規制されており、アマゾンギフト券との交換にとどまっています。`
         },
         registerAsPrepaidWithSettlement: {
@@ -78,8 +78,8 @@ LCNEM Wallet connects the Google Account with the private key, so it is possible
             ja: "第三者型前払式支払手段発行業登録"
         },
         registerAsPrepaidWithSettlementTooltip: {
-            en: "By registering the issuing as third party type prepaid payment method , it is possible to respond to cash to the store with the contract.
-            Because the government ordinance that the net asset of 100 million yen or more is required for registration is specified, we will need fund raising.",
+            en: `By registering the issuing as third party type prepaid payment method , it is possible to respond to cash to the store with the contract.
+            Because the government ordinance that the net asset of 100 million yen or more is required for registration is specified, we will need fund raising.`,
             ja: `第三者型前払式支払手段発行業登録をすることにより、契約を結んだ店舗への換金対応が可能になります。
 登録には、純資産1億円以上が必要であるとの政令が指定されているため、資金調達を行います。`
         },
@@ -88,8 +88,8 @@ LCNEM Wallet connects the Google Account with the private key, so it is possible
             ja: "資金移動業登録"
         },
         registerAsRemittanceTooltip: {
-            en: "Third party type prepaid payment method Issuing business registration creates a practical system, but in order to pursue even more convenience, we are looking at registration of funds transfer business.
-            Refund will be possible by fund transfer business registration.",
+            en: `Third party type prepaid payment method Issuing business registration creates a practical system, but in order to pursue even more convenience, we are looking at registration of funds transfer business.
+            Refund will be possible by fund transfer business registration.`,
             ja: `第三者型前払式支払手段発行業登録により実用的なシステムが完成しますが、さらなる利便性の追求のため資金移動業登録を見据えています。
 資金移動業登録により、払戻しが可能になります。`
         },
