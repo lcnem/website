953c7863ef0c6e709d00387b1240244f7b86fc68
Ryotaro Miyagwa <o_o_o_o_o1031@yahoo.co.jp>
2018-11-04 15:09:10 +0900

    side-navコンポーネント化

--- angular/src/app/components/side-nav/side-nav.component.ts
+++ angular/src/app/components/side-nav/side-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GlobalDataService } from '../../services/global-data.service';
 
 @Component({
   selector: 'app-side-nav',
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(public global: GlobalDataService) {}
 
   ngOnInit() {
   }
+
+  public translation = {
+    contacts: {
+      en: "Contacts",
+      ja: "問い合わせ"
+    } as any,
+    lcnemWallet: {
+      en: "LCNEM Wallet",
+      ja: "LCNEM Wallet"
+    } as any,
+    ticketP2p: {
+      en: "Ticket Peer to Peer",
+      ja: "ちけっとピアツーピア"
+    } as any,
+    recruiteInformation: {
+      en: "RecruiteInformation",
+      ja: "採用情報"
+    } as any,
+    aboutCompany: {
+      en: "Corporate Information",
+      ja: "会社概要"
+    } as any,
+  };
 
 }
