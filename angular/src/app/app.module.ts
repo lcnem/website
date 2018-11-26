d048ae51f9bc74a6304035698c5e7eab0cbc398e
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-11-26 12:09:07 +0900

    Feature/subsidy (#121)
    * [add]緊急時連署名サービス
    
    * [update]ngrx-language
    
    * [add]subsidy

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -39,11 +39,11 @@ import { HeaderComponent } from './components/header/header.component';
 import { SideNavComponent } from './components/side-nav/side-nav.component';
 import { BodyContainerComponent } from './containers/body-container/body-container.component';
 import { EmergencyCosignComponent } from './others/emergency-cosign/emergency-cosign.component';
-
 import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
+import { SubsidyComponent } from './subsidy/subsidy.component';
 
 @NgModule({
   declarations: [
@@ -61,6 +61,8 @@ import { environment } from '../environments/environment';
     HeaderComponent,
     SideNavComponent,
     BodyContainerComponent,
+    EmergencyCosignComponent,
+    SubsidyComponent,
     EmergencyCosignComponent
   ],
   imports: [
