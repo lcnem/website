795018d0262c027094e33033b2d250b7a241558a
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2018-12-10 22:46:35 +0900

    [update]

--- angular/src/app/services/language.service.ts
+++ angular/src/app/services/language.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from '@angular/core';
-import { RxStateStore } from 'rx-state-store-js';
+import { RxStateStore } from 'rx-state-store-js'
 
 @Injectable({
   providedIn: 'root'
 })
 export class LanguageService extends RxStateStore<State> {
   constructor() {
-    super({
-      twoLetter: window.navigator.language.substr(0, 2) == "ja" ? "ja" : "en"
-    })
+    super(
+      {
+        twoLetter: window.navigator.language.substr(0, 2) == "ja" ? "ja" : "en"
+      }
+    )
   }
-
-  public get twoLetter() { return this._state.twoLetter; }
 
   public setLanguage(twoLetter: string) {
-    this.streamState({twoLetter})
+    this.streamState(
+      {
+        ...this._state,
+        twoLetter: twoLetter
+      }
+    )
   }
+  
+  public get twoLetter() { return this._state.twoLetter; }
 }
 
 interface State {
