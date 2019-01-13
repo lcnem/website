7d8e90e0c53a97b0c3c0540c74da02c3c5651ebe
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2019-01-13 14:31:20 +0900

    [update]e->error

--- angular/src/app/pages/company/contact/contact.component.ts
+++ angular/src/app/pages/company/contact/contact.component.ts
@@ -68,7 +68,7 @@ export class ContactComponent implements OnInit {
         )
         this.forms = {} as any
       },
-      (e) => {
+      (error) => {
         this.dialog.open(
           AlertDialogComponent,
           {
