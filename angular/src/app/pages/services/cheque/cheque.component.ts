3826cfc0552bad5c3696fe83ea37ddd379a57ae9
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2019-01-28 19:59:34 +0900

    1/28 (#184)
    * tableの修正・iconの修正
    
    * 余白の調整
    
    * contactへの導線
    
    * [fix]_blank
    
    * [update]
    
    * Feature/contact (#165)
    
    * tableの修正・iconの修正
    
    * 余白の調整
    
    * contactへの導線
    
    * [fix]_blank
    
    * [update]
    
    * [update]walletページ
    
    * Update issue templates (#168)
    
    * [update]
    
    * [update]
    
    * language_serviceの修正
    
    * [update]filter
    
    * [update]e->error
    
    * [update] (#170)
    
    * [update]
    
    * language_serviceの修正
    
    * [update]filter
    
    * [update]e->error
    
    * [update]routing
    
    * Feature/routing (#174)
    
    * [update]
    
    * language_serviceの修正
    
    * [update]filter
    
    * [update]e->error
    
    * [update]routing
    
    * [update]routing-services
    
    * [update]loading-dialog
    
    * Feature/services (#175)
    
    * [update]
    
    * language_serviceの修正
    
    * [update]filter
    
    * [update]e->error
    
    * [update]routing
    
    * [update]routing-services
    
    * [update]loading-dialog (#177)
    
    * [update]revise
    
    * [update]
    
    * [delete]conflict
    
    * [fix]
    
    * [update]cheque
    
    * [update]cheque (#180)
    
    * [update]home-translate-english (#182)
    
    * [update]

--- angular/src/app/pages/services/cheque/cheque.component.ts
+++ angular/src/app/pages/services/cheque/cheque.component.ts
@@ -17,14 +17,6 @@ export class ChequeComponent implements OnInit {
   }
 
   public translation = {
-    questionSubsidy: {
-      en: "How to implement for you app",
-      ja: "アプリへの組み込みはどうやるのですか？"
-    } as any,
-    answerSubsidy: {
-      en: "You implement easily by using NEM blockchain.",
-      ja: "NEMブロックチェーン使うことで簡単にできます。"
-    } as any,
     abountSubsidy: {
       en: "",
       ja: "助成金について"
@@ -32,6 +24,33 @@ export class ChequeComponent implements OnInit {
     show: {
       en: "",
       ja: "のページをご覧ください。"
+    } as any,
+    roadmap: {
+      en: "Roadmap of LCNEM Cheque",
+      ja: "LCNEM Chequeのロードマップ"
+    } as any,
+    noActionLetter: {
+      en: "Preliminary confirmation procedure to the Financial Services Agency",
+      ja: "金融庁への法令適用事前確認手続き"
+    } as any,
+    issueAsPrepaid: {
+      en: "Started issuing as a self-contained prepaid payment method",
+      ja: "自家型前払式支払手段として発行開始"
+    } as any,
+    issueAsPrepaidBody: {
+      en: `We will start issuing as a self-contained prepaid payment method. In \"self-contained prepaid payment method\", the cash is restricted by law and it has been only in exchange with Amazon gift certificates.`,
+      ja: `自家型前払式支払手段として、発行を開始します。「自家型前払式支払手段」では、法律により換金が規制されており、アマゾンギフト券との交換にとどまっています。`
+    } as any,
+    registerAsPrepaidWithSettlement: {
+      en:
+        "Prepaid payment method with settlement system for third party stores Registration",
+      ja: "第三者型前払式支払手段発行業登録"
+    } as any,
+    registerAsPrepaidWithSettlementBody: {
+      en: `By registering the issuing as third party type prepaid payment method, it is possible to respond to cash to the store with the contract.
+Because the government ordinance that the net asset of 100 million yen or more is required for registration is specified, we will need fund raising.`,
+      ja: `第三者型前払式支払手段発行業登録をすることにより、契約を結んだ店舗への換金対応が可能になります。
+登録には、純資産1億円以上が必要であるとの政令が指定されているため、資金調達を行います。`
     } as any
   }
 
