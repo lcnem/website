c05b84a55434489f15b9d04766f43916cc6eea36
Yu Kimura <33382781+YuKimura45z@users.noreply.github.com>
2019-03-01 21:45:46 +0900

    Recruit (#205)
    * [update]link
    
    * [delete]package
    
    * [update]lint
    
    * [update]package
    
    * [update]recruit

--- angular/src/app/services/language/language.service.spec.ts
+++ angular/src/app/services/language/language.service.spec.ts
@@ -3,10 +3,10 @@ import { TestBed } from '@angular/core/testing';
 import { LanguageService } from './language.service';
 
 describe('LanguageService', () => {
-  // beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({}));
 
-  // it('should be created', () => {
-  //   const service: LanguageService = TestBed.get(LanguageService);
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    const service: LanguageService = TestBed.get(LanguageService);
+    expect(service).toBeTruthy();
+  });
 });
