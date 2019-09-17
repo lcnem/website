7ae9cce68d9091e107046cf3346f5f24061b123c
user <hiroaking717@gmail.com>
2019-09-17 09:36:24 +0900

    [update]コンポーネント名修正

--- angular/src/app/company/subscription-privacy-policy/subscription-privacy-policy.component.spec.ts
+++ angular/src/app/company/subscription-privacy-policy/subscription-privacy-policy.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PrivacyPolicyComponent } from './subscription-privacy-policy.component';
+import { SubscriptionPrivacyPolicyComponent } from './subscription-privacy-policy.component';
 
 describe('PrivacyPolicyComponent', () => {
   // let component: PrivacyPolicyComponent;
