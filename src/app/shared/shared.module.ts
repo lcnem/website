ee850a6da5ac6462080d2495d446349ebe66f484
user <hiroaking717@gmail.com>
2020-01-21 11:02:22 +0900

    [update]header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,20 @@ import { CommonModule } from '@angular/common';
 import { LoadingDialogComponent } from './loading-dialog/loading-dialog.component';
 import { TwitterTimelineComponent } from './twitter-timeline/twitter-timeline.component';
 import { HeaderComponent } from './header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 
 @NgModule({
   declarations: [LoadingDialogComponent, TwitterTimelineComponent, HeaderComponent],
   imports: [
-    CommonModule
+    FlexLayoutModule,
+    CommonModule,
   ],
-  exports: [TwitterTimelineComponent]
+  exports: [
+    TwitterTimelineComponent,
+    HeaderComponent,
+    LoadingDialogComponent,
+  ]
 })
 export class SharedModule { }
