1135763f0971f00b64dcb96e489ffd551a77e6ac
user <hiroaking717@gmail.com>
2020-01-22 19:29:21 +0900

    [update]about

---
+++ src/app/services/firebase-migration/firebase-migration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firebase-migration',
+  templateUrl: './firebase-migration.component.html',
+  styleUrls: ['./firebase-migration.component.css']
+})
+export class FirebaseMigrationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -5,10 +5,11 @@ import { ServicesRoutingModule } from './services-routing.module';
 import { BlockchainSubscriptionComponent } from './blockchain-subscription/blockchain-subscription.component';
 import { SystemIntegrationComponent } from './system-integration/system-integration.component';
 import { MinimumViableProductComponent } from './minimum-viable-product/minimum-viable-product.component';
+import { FirebaseMigrationComponent } from './firebase-migration/firebase-migration.component';
 
 
 @NgModule({
-  declarations: [BlockchainSubscriptionComponent, SystemIntegrationComponent, MinimumViableProductComponent],
+  declarations: [BlockchainSubscriptionComponent, SystemIntegrationComponent, MinimumViableProductComponent, FirebaseMigrationComponent],
   imports: [
     CommonModule,
     ServicesRoutingModule
