9b44ca6c2302410a2fb0b466cd0d906c03a8d12a
user <hiroaking717@gmail.com>
2020-01-23 12:24:35 +0900

    [update]サービス途中

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,26 +9,26 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
-  public services = [
-    {
-      name: 'Blockchain Subscription',
-      link: 'services/blockchain-subscription',
-      image: 'assets/images/development.jpg',
-      copy: 'サブスクリプション型のブロックチェーン開発サービス',
-    } as any,
-    {
-      name: 'Minimum Viable Product',
-      link: 'services/minimum-viable-product',
-      image: 'assets/images/consulting.jpg',
-      copy: 'MVP開発設計参加&コンサルティングサービス',
-    } as any,
-    {
-      name: 'System Integration',
-      link: 'services/minimum-viable-product',
-      image: 'assets/images/development.jpg',
-      copy: 'システム開発受託',
-    } as any,
-  ];
+  // public services = [
+  //   {
+  //     name: 'Blockchain Subscription',
+  //     link: 'services/blockchain-subscription',
+  //     image: 'assets/images/development.jpg',
+  //     copy: 'サブスクリプション型のブロックチェーン開発サービス',
+  //   } as any,
+  //   {
+  //     name: 'Minimum Viable Product',
+  //     link: 'services/minimum-viable-product',
+  //     image: 'assets/images/consulting.jpg',
+  //     copy: 'MVP開発設計参加&コンサルティングサービス',
+  //   } as any,
+  //   {
+  //     name: 'System Integration',
+  //     link: 'services/minimum-viable-product',
+  //     image: 'assets/images/development.jpg',
+  //     copy: 'システム開発受託',
+  //   } as any,
+  // ];
 
   public partners = [
     {
