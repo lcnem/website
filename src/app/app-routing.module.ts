9ffa251183be72a343e125b69ea2c0764688d292
user <hiroaking717@gmail.com>
2020-01-29 15:12:08 +0900

    [update]シングルクリックで移動

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    scrollPositionRestoration: 'enabled', // enabled で有効化
+    anchorScrolling: 'enabled', // enabled で有効化)],
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
