b973b4b948cb209f64aee7c6499b64b36a71e776
Kimura Yu <33382781+KimuraYu45z@users.noreply.github.com>
2021-03-29 16:22:02 +0900

    chore: npm script

--- functions/bundle.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const sources = ['package-lock.json', 'package.json'];
-
-for (const source of sources) {
-  fs.copyFileSync(`../shared/${source}`, `shared/${source}`);
-}
